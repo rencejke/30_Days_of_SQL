14ae336722e9e6de49853c61845982fdc8e7af50
rencejke <bonillaclarencejake@gmail.com>
2025-05-06 02:37:21 +0800

    Day 2 complete

--- Day2/2.sql
+++ Day2/2.sql
@@ -1,3 +1,9 @@
+--0Retrieve all employees whose age is less than 30.
+--Find employees in the Marketing department or aged 35 and above.
+--Retrieve employees whose names end with the letter 'n'.
+--List employees who do not belong to the IT department.
+--Retrieve employees with non-NULL age values.
+
 --1
 SELECT * FROM employee WHERE age < 30
 
